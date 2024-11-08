5e09060da3ffcaf1f20a577900fdad365e5ba4e7
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-08 13:14:28 +0200

    fix

--- src/controllers/updateControllers.ts
+++ src/controllers/updateControllers.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 
-const postFavorites = catchAsync(async (req: Request, res: Response) => {});
+const addFavorites = catchAsync(async (req: Request, res: Response) => {});
 
 const deleteFavorites = catchAsync(async (req: Request, res: Response) => {});
 
 export default {
   deleteFavorites,
-  postFavorites,
+  addFavorites,
 };
