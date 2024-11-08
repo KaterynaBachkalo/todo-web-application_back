62a2f3487e665705cc704456d3d69bb5ca14741d
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-08 13:14:15 +0200

    fix registration

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
+import userServices from "../services/userServices";
 
-const registration = catchAsync(async (req: Request, res: Response) => {});
+const registration = catchAsync(async (req: Request, res: Response) => {
+  const { user } = await userServices.registration(req.body);
+
+  res.status(201).json({
+    user: { email: user.email },
+  });
+});
 
 const login = catchAsync(async (req: Request, res: Response) => {});
 
