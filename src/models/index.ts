5b2ed4fa7d9a753d46d5f8c9fee72366bd9b85cc
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-11 17:05:06 +0200

    fix name

---
+++ src/models/index.ts
@@ -0,0 +1,8 @@
+import { AddPet } from "./addPetModel";
+import { City } from "./cityModel";
+import { Friend } from "./friendModel";
+import { News } from "./newsModel";
+import { Notice } from "./noticeModel";
+import { User } from "./userModel";
+
+export { AddPet, User, City, Friend, News, Notice };
