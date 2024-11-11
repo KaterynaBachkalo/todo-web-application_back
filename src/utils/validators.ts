6653aa62f7a5246d4b4a84714edab4c219a3c655
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-11 17:06:14 +0200

    fix schema

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,9 +1,7 @@
 import Joi from "joi";
 
 const addUserSchema = Joi.object({
-  name: Joi.string().min(6).required().messages({
-    "any.required": "Set name for user",
-  }),
+  name: Joi.string().min(1).allow("").default("NONAME"),
 
   password: Joi.string().min(6).required().messages({
     "any.required": "Set password for user",
