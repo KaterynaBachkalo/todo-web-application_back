196e67b096e9144ff4d9d8b3fd387843b270d146
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-11 17:05:58 +0200

    add login service

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,12 +1,19 @@
+import serverConfig from "../configs/serverConfig";
 import { User } from "../models/userModel";
 import HttpError from "../utils/HttpError";
+import jwt from "jsonwebtoken";
 
 interface registrationData {
-  userData: {
+  user: {
     name: string;
     email: string;
     password: string;
   };
+}
+
+interface LoginData {
+  email: string;
+  password: string;
 }
 
 const checkUserEmailExists = async (email: string) => {
@@ -29,7 +36,37 @@ const registration = async (data: registrationData) => {
   };
 };
 
+const login = async ({ email, password }: LoginData) => {
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) throw new HttpError(401, "Email or password is wrong");
+
+  const passwdIsValid = await user.checkPassword(password, user.password);
+
+  if (!passwdIsValid) throw new HttpError(401, "Email or password is wrong");
+
+  user.password = "";
+
+  const accessToken = jwt.sign({ id: user.id }, serverConfig.jwtSecret, {
+    expiresIn:
+      process.env.NODE_ENV === "production" ? "120m" : serverConfig.jwtExpires,
+  });
+
+  const refreshToken = jwt.sign({ id: user.id }, serverConfig.jwtSecret, {
+    expiresIn: process.env.NODE_ENV === "production" ? "7d" : "1d",
+  });
+
+  await User.findByIdAndUpdate(user.id, { accessToken, refreshToken });
+
+  return {
+    user: { email: user.email },
+    accessToken,
+    refreshToken,
+  };
+};
+
 export default {
   checkUserEmailExists,
   registration,
+  login,
 };
