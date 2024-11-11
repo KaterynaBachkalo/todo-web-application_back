07c802dde69b2ee5db38e53cd7ff863c4ae2b7bf
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-11 17:04:29 +0200

    add checkLoginData, add cheking name and add protect function

--- src/middlewares/authMiddlewares.ts
+++ src/middlewares/authMiddlewares.ts
@@ -1,6 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import userServices from "../services/userServices";
 import { catchAsync, HttpError, validSchemas } from "../utils";
+import { jwtServices, userServices } from "../services";
+import { User } from "../models";
+
+interface MyCustomRequest extends Request {
+  user?: any;
+}
 
 const checkRegistrationData = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -10,6 +15,8 @@ const checkRegistrationData = catchAsync(
 
     await userServices.checkUserEmailExists(value.email);
 
+    if (value.name === "") value.name = "NONAME";
+
     req.body = value;
 
     next();
@@ -18,12 +25,33 @@ const checkRegistrationData = catchAsync(
 
 const checkLoginData = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
+    const { value, error } = validSchemas.addUserSchema.validate(req.body);
+
+    if (error) throw new HttpError(400, error.message);
+
+    req.body = value;
+
     next();
   }
 );
 
 const protect = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: MyCustomRequest, res: Response, next: NextFunction) => {
+    const accessToken =
+      req.headers.authorization?.startsWith("Bearer ") &&
+      req.headers.authorization.split(" ")[1];
+
+    const userId = accessToken && jwtServices.checkToken(accessToken);
+
+    if (!userId) throw new HttpError(401, "Not authorized");
+
+    const currentUser = await User.findById(userId);
+
+    if (!currentUser || !currentUser.accessToken)
+      throw new HttpError(401, "Not authorized");
+
+    req.user = currentUser;
+
     next();
   }
 );
