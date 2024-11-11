ddcc5e189032b7ff1f86bccec93a3a35ba5f2db8
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-11 17:03:33 +0200

    add login and logout

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,20 +1,38 @@
 import { Request, Response } from "express";
-import catchAsync from "../utils/catchAsync";
-import userServices from "../services/userServices";
+import { User } from "../models";
+import { ObjectId } from "mongodb";
+import { userServices } from "../services";
+import { catchAsync } from "../utils";
+
+interface CustomRequest extends Request {
+  user: { _id: ObjectId; email: string; name: string };
+}
 
 const registration = catchAsync(async (req: Request, res: Response) => {
   const { user } = await userServices.registration(req.body);
 
-  res.status(201).json({
-    user: { email: user.email },
-  });
+  res.status(201).json({ user: { email: user.email, name: user.name } });
 });
 
-const login = catchAsync(async (req: Request, res: Response) => {});
+const login = catchAsync(async (req: Request, res: Response) => {
+  const { user, accessToken, refreshToken } = await userServices.login(
+    req.body
+  );
+
+  res
+    .status(200)
+    .json({ user: { email: user.email, accessToken, refreshToken } });
+});
 
 const refreshToken = catchAsync(async (req: Request, res: Response) => {});
 
-const logout = catchAsync(async (req: Request, res: Response) => {});
+const logout = catchAsync(async (req: CustomRequest, res: Response) => {
+  const { _id } = req.user;
+
+  await User.findByIdAndUpdate(_id, { accessToken: "", refreshToken: "" });
+
+  res.status(204).json();
+});
 
 export default {
   registration,
