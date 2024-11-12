3e4f7aafbcae250044087ffe05a7a020332bccae
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-12 20:44:35 +0200

    fix authController

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -19,9 +19,9 @@ const login = catchAsync(async (req: Request, res: Response) => {
     req.body
   );
 
-  res
-    .status(200)
-    .json({ user: { email: user.email, accessToken, refreshToken } });
+  res.status(200).json({
+    user: { email: user.email, name: user.name, accessToken, refreshToken },
+  });
 });
 
 const refreshToken = catchAsync(async (req: Request, res: Response) => {});
