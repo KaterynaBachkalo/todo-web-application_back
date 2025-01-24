0875ba87f60e904dbda7e0ab294ea0bfd2d743ac
KaterynaBachkalo <kozlovets88@gmail.com>
2025-01-24 13:06:44 +0200

    add getCurrentUserInfo

--- src/controllers/getAuthControllers.ts
+++ src/controllers/getAuthControllers.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
+import { ObjectId } from "mongoose";
+
+interface CustomRequest extends Request {
+  user: { _id: ObjectId; email: string; name: string };
+}
 
 const getCurrentUserInfo = catchAsync(
-  async (req: Request, res: Response) => {}
+  async (req: CustomRequest, res: Response) => {
+    const { email, name } = req.user;
+    res.status(200).json({ email, name });
+  }
 );
 
 const getCurrentUserFullInfo = catchAsync(
