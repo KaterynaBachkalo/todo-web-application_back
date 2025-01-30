073ebad206cbf50a3f563f695e422728eaed3d6b
KaterynaBachkalo <kozlovets88@gmail.com>
2025-01-30 12:21:24 +0200

    add checkFavoritesId

--- src/middlewares/updateMiddlewares.ts
+++ src/middlewares/updateMiddlewares.ts
@@ -1,19 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
+import { Favorite } from "../models";
+import { HttpError } from "../utils";
 
 const checkFavoritesId = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    next();
-  }
-);
+    const { id } = req.params;
 
-const checkAddFavorites = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+    const idExists = await Favorite.exists({ _id: id });
+
+    if (idExists) throw new HttpError(409, "Favorite already exists");
+
     next();
   }
 );
 
 export default {
   checkFavoritesId,
-  checkAddFavorites,
 };
