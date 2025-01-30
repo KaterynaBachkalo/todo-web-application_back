6992b4ffb94a5e8ea537ecc95dd4c3640be3ed4c
KaterynaBachkalo <kozlovets88@gmail.com>
2025-01-30 12:19:28 +0200

    add favorites

--- src/controllers/getAuthControllers.ts
+++ src/controllers/getAuthControllers.ts
@@ -3,13 +3,13 @@ import catchAsync from "../utils/catchAsync";
 import { ObjectId } from "mongoose";
 
 interface CustomRequest extends Request {
-  user: { _id: ObjectId; email: string; name: string };
+  user: { _id: ObjectId; email: string; name: string; favorites: string[] };
 }
 
 const getCurrentUserInfo = catchAsync(
   async (req: CustomRequest, res: Response) => {
-    const { email, name } = req.user;
-    res.status(200).json({ email, name });
+    const { email, name, favorites } = req.user;
+    res.status(200).json({ email, name, favorites });
   }
 );
 
