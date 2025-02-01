9a80f05ac9afa9c3d2114a970876cb127907eb33
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-01 18:06:30 +0200

    add favorite controllers

--- src/controllers/updateControllers.ts
+++ src/controllers/updateControllers.ts
@@ -1,9 +1,59 @@
 import { Request, Response } from "express";
+import { ObjectId } from "mongoose";
+
 import catchAsync from "../utils/catchAsync";
+import { Favorite, User } from "../models";
+import { HttpError } from "../utils";
+import { userServices } from "../services";
 
-const addFavorites = catchAsync(async (req: Request, res: Response) => {});
+interface CustomRequest extends Request {
+  user: { _id: ObjectId; email: string; name: string; favorites: string };
+}
 
-const deleteFavorites = catchAsync(async (req: Request, res: Response) => {});
+const addFavorites = catchAsync(async (req: CustomRequest, res: Response) => {
+  const { id } = req.params;
+
+  const userId = req.user._id;
+
+  if (!userId) {
+    throw new HttpError(401, "Unauthorized");
+  }
+
+  const favorites = await userServices.updateFavoriteId(userId, id);
+
+  await Favorite.create({ _id: id, owner: userId });
+
+  res.status(201).json(favorites);
+});
+
+const deleteFavorites = catchAsync(
+  async (req: CustomRequest, res: Response) => {
+    const { id } = req.params;
+
+    const userId = req.user._id;
+
+    if (!userId) {
+      throw new HttpError(401, "Unauthorized");
+    }
+
+    await userServices.updateFavoriteId(userId, id);
+
+    const deletedFavorite = await Favorite.findOneAndDelete({
+      _id: id,
+      owner: userId,
+    });
+
+    if (!deletedFavorite) {
+      throw new HttpError(404, "Favorite not found");
+    }
+
+    await User.findByIdAndUpdate(userId, { $pull: { favorites: id } });
+
+    res
+      .status(200)
+      .json({ message: "The favorite pet is successfully deleted" });
+  }
+);
 
 export default {
   deleteFavorites,
