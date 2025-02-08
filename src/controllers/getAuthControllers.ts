6dad55f254e1abf0716024f703f1de9a06e8a9e3
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-08 17:25:42 +0200

    fix

--- src/controllers/getAuthControllers.ts
+++ src/controllers/getAuthControllers.ts
@@ -3,13 +3,19 @@ import catchAsync from "../utils/catchAsync";
 import { ObjectId } from "mongoose";
 
 interface CustomRequest extends Request {
-  user: { _id: ObjectId; email: string; name: string; favorites: string[] };
+  user: {
+    _id: ObjectId;
+    email: string;
+    name: string;
+    favorites: string[];
+    avatar: string;
+  };
 }
 
 const getCurrentUserInfo = catchAsync(
   async (req: CustomRequest, res: Response) => {
-    const { email, name, favorites } = req.user;
-    res.status(200).json({ email, name, favorites });
+    const { email, name, favorites, avatar } = req.user;
+    res.status(200).json({ email, name, favorites, avatar });
   }
 );
 
