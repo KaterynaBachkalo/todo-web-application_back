d1b4af243d6100707332c78fbc8469ad13af7115
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-16 23:11:58 +0200

    add

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import userServices from "./userServices";
 import jwtServices from "./jwtServices";
 import petServices from "./petServices";
+import { upload, createAvatar } from "./avatarServices";
 
-export { userServices, jwtServices, petServices };
+export { userServices, jwtServices, petServices, upload, createAvatar };
