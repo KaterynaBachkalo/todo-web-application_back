145617d8c756649bf86c16bdbebb0ece2388460f
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-16 23:11:11 +0200

    fix

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import logger from "morgan";
 import cors from "cors";
 import { NextFunction, Request, Response } from "express";
+import path from "path";
 
 import {
   noticesRouter,
@@ -21,7 +22,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use("/api/users", authRouter);
 app.use("/api/notices", noticesRouter);
