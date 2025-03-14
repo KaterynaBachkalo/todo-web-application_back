32abd5588095afe0c4177a33dfdf38be9afbe1c2
KaterynaBachkalo <kozlovets88@gmail.com>
2025-03-14 13:43:30 +0200

    del

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,7 +1,6 @@
 import noticesRouter from "./noticesRouter";
-import citiesRouter from "./citiesRouter";
 import friendsRouter from "./friendsRouter";
 import newsRouter from "./newsRouter";
 import authRouter from "./authRouter";
 
-export { noticesRouter, citiesRouter, friendsRouter, newsRouter, authRouter };
+export { noticesRouter, friendsRouter, newsRouter, authRouter };
