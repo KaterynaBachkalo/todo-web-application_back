d6281985a451eaad1d48484be439f46c48178f4c
KaterynaBachkalo <kozlovets88@gmail.com>
2025-05-08 13:18:45 +0300

    add cloudinary items

--- src/configs/serverConfig.ts
+++ src/configs/serverConfig.ts
@@ -7,6 +7,9 @@ const serverConfig = {
   jwtSecret: process.env.JWT_SECRET ?? "secret-phrase",
   jwtExpires: process.env.JWT_EXPIRES ?? "20h",
   baseUrl: process.env.BASE_URL ?? "http://localhost:4000",
+  cloudinaryName: process.env.CLOUDINARY_NAME ?? "cloudinaryName",
+  cloudinaryKey: process.env.CLOUDINARY_KEY ?? "12345",
+  cloudinarySecret: process.env.CLOUDINARY_SECRET ?? "cloudinarySecret",
 };
 
 export default serverConfig;
