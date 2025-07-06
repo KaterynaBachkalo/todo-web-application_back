fcf59dfca126ab3e1e8de190b1e5226ec6aabf14
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-06 16:43:05 +0300

    add getContacts

--- src/controllers/getControllers.ts
+++ src/controllers/getControllers.ts
@@ -1,41 +1,18 @@
 import { Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
-import { petServices } from "../services";
+import { contactServices } from "../services";
 
-const getNotices = catchAsync(async (req: Request, res: Response) => {
-  const { notices, totalNotices, page, limit } = await petServices.getNotices(
-    req.query
-  );
-
+const getContacts = catchAsync(async (req: Request, res: Response) => {
+  const { contacts, totalContacts, page, limit } =
+    await contactServices.getContacts(req.query);
   res.status(200).json({
-    notices,
-    totalNotices,
-    page,
-    limit,
-  });
-});
-
-const getFriends = catchAsync(async (req: Request, res: Response) => {
-  const { friends } = await petServices.getFriends();
-
-  res.status(200).json({
-    friends,
-  });
-});
-
-const getNews = catchAsync(async (req: Request, res: Response) => {
-  const { news, totalNews, page, limit } = await petServices.getNews(req.query);
-
-  res.status(200).json({
-    news,
-    totalNews,
+    contacts,
+    totalContacts,
     page,
     limit,
   });
 });
 
 export default {
-  getNotices,
-  getFriends,
-  getNews,
+  getContacts,
 };
