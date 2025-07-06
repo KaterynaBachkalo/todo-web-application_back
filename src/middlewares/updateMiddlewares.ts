dcfe166f75d4ec5e9ea4c93398a612156d7d2feb
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-06 16:45:02 +0300

    add checkContactId

--- src/middlewares/updateMiddlewares.ts
+++ src/middlewares/updateMiddlewares.ts
@@ -1,37 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
-import { Favorite, Notice, Viewed } from "../models";
+import { Contact } from "../models";
 import { HttpError } from "../utils";
 
-const checkFavoritesId = catchAsync(
+const checkContactId = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
 
-    const idExists = await Favorite.exists({ _id: id });
-
-    if (idExists) throw new HttpError(409, "Favorite already exists");
-
-    next();
-  }
-);
-
-const checkViewedId = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
-
-    const idExists = await Viewed.exists({ _id: id });
-
-    if (idExists) throw new HttpError(409, "Favorite already exists");
-
-    next();
-  }
-);
-
-const checkNoticesId = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
-
-    const idExists = await Notice.exists({ _id: id });
+    const idExists = await Contact.findByPk(id);
 
     if (idExists) throw new HttpError(409, "Notice already exists");
 
@@ -40,7 +16,5 @@ const checkNoticesId = catchAsync(
 );
 
 export default {
-  checkFavoritesId,
-  checkViewedId,
-  checkNoticesId,
+  checkContactId,
 };
