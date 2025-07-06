1f2b51ef2fb25e7f5e0bd83a4fed520fe0c06282
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-06 16:44:31 +0300

    addToContacts,   deleteFromContacts

--- src/controllers/updateControllers.ts
+++ src/controllers/updateControllers.ts
@@ -1,129 +1,52 @@
 import { Request, Response } from "express";
-import { ObjectId } from "mongoose";
 
 import catchAsync from "../utils/catchAsync";
-import { AddPet, Favorite, Notice, User, Viewed } from "../models";
+import { Contact } from "../models";
 import { HttpError } from "../utils";
-import { userServices } from "../services";
+import { CreationOptional } from "sequelize";
 
 interface CustomRequest extends Request {
   user: {
-    _id: ObjectId;
+    id: CreationOptional<number>;
     email: string;
     name: string;
-    favorites: string[];
-    viewed: string[];
   };
 }
 
-const checkAuthorization = (userId: ObjectId) => {
-  if (!userId) {
-    throw new HttpError(401, "Unauthorized");
-  }
-};
-
-const addFavorites = catchAsync(async (req: CustomRequest, res: Response) => {
+const addToContacts = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  const userId = req.user._id;
+  const myContact = await Contact.findByPk(id);
 
-  checkAuthorization(userId);
-
-  const favorites = await userServices.updateFavoriteId(userId, id);
-
-  await Favorite.create({ _id: id, owner: userId });
-
-  res.status(201).json(favorites);
-});
-
-const addViewed = catchAsync(async (req: CustomRequest, res: Response) => {
-  const { id } = req.params;
-
-  const userId = req.user._id;
-
-  checkAuthorization(userId);
-
-  const viewed = await userServices.updateViewedId(userId, id);
-
-  await Viewed.create({ _id: id, owner: userId });
-
-  res.status(201).json(viewed);
-});
-
-const deleteFavorites = catchAsync(
-  async (req: CustomRequest, res: Response) => {
-    const { id } = req.params;
-
-    const userId = req.user._id;
-
-    checkAuthorization(userId);
-
-    await userServices.updateFavoriteId(userId, id);
-
-    const deletedFavorite = await Favorite.findOneAndDelete({
-      _id: id,
-      owner: userId,
-    });
-
-    if (!deletedFavorite) {
-      throw new HttpError(404, "Favorite not found");
-    }
-
-    await User.findByIdAndUpdate(userId, { $pull: { favorites: id } });
-
-    res
-      .status(200)
-      .json({ message: "The favorite pet is successfully deleted" });
-  }
-);
-
-const addToNotices = catchAsync(async (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  const myPet = await AddPet.findById(id);
-
-  if (!myPet) {
-    return res.status(404).json({ message: "Pet not found" });
+  if (!myContact) {
+    return res.status(404).json({ message: "Contact not found" });
   }
 
-  const notice = new Notice({
-    _id: myPet._id,
-    name: myPet.name,
-    title: myPet.title,
-    imgURL: myPet.imgURL,
-    species: myPet.species,
-    birthday: myPet.birthday,
-    sex: myPet.sex,
-    owner: myPet.owner,
+  const contact = await Contact.create({
+    name: myContact.name,
+    phone: myContact.phone,
   });
 
-  await notice.save();
-
-  res.status(201).json(notice);
+  res.status(201).json(contact);
 });
 
-const deleteMyPetFromNotice = catchAsync(
+const deleteFromContacts = catchAsync(
   async (req: CustomRequest, res: Response) => {
     const { id } = req.params;
 
-    const deletedPet = await Notice.findOneAndDelete({
-      _id: id,
-    });
+    const deletedContact = await Contact.destroy({ where: { id: id } });
 
-    if (!deletedPet) {
-      throw new HttpError(404, "Favorite not found");
+    if (!deletedContact) {
+      throw new HttpError(404, "Contact not found");
     }
 
     res.status(200).json({
-      message: "Your pet is successfully removed from publication",
+      message: "Your contact is successfully removed",
     });
   }
 );
 
 export default {
-  deleteFavorites,
-  addFavorites,
-  addViewed,
-  addToNotices,
-  deleteMyPetFromNotice,
+  addToContacts,
+  deleteFromContacts,
 };
