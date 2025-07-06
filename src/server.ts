bc63d0a6e135fe9b0ed0dd718b2df0cd230f4ca8
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-06 16:19:10 +0300

    add app.listen

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,6 @@
 import app from "./app";
-
 import serverConfig from "./configs/serverConfig";
-import mongoose from "mongoose";
-
-mongoose
-  .connect(serverConfig.mongoUrl)
-  .then(() => {
-    console.log("Database connection successful");
-  })
-  .catch((er: any) => {
-    console.log(er.message);
-    process.exit(1);
-  });
 
 app.listen(serverConfig.PORT, () => {
-  console.log("Server running.");
+  console.log("Server running to mariadb");
 });
