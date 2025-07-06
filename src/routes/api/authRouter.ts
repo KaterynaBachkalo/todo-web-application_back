326db9f9bcea837eb71725c921fe7db30cdd6c62
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-06 16:47:22 +0300

    add auth routers

--- src/routes/api/authRouter.ts
+++ src/routes/api/authRouter.ts
@@ -1,12 +1,6 @@
 import express from "express";
 import { authMiddlewares } from "../../middlewares";
-import {
-  authControllers,
-  getAuthControllers,
-  google,
-  updateAuthControllers,
-} from "../../controllers";
-import { upload } from "../../services/avatarServices";
+import { authControllers, getAuthControllers, google } from "../../controllers";
 
 const router = express.Router();
 
@@ -22,32 +16,9 @@ router
   .route("/login")
   .post(authMiddlewares.checkLoginData, authControllers.login);
 
-router.post("/refresh-token", authControllers.refreshToken);
-
 router.use(authMiddlewares.protect);
 
 router.route("/current").get(getAuthControllers.getCurrentUserInfo);
-
-router
-  .route("/current/edit")
-  .patch(
-    authMiddlewares.checkCurrentUser,
-    updateAuthControllers.updateCurrentUser
-  );
-
-router.patch("/avatars", upload.single("avatar"), authControllers.updateAvatar);
-
-router
-  .route("/current/pets/add")
-  .post(
-    upload.single("imgURL"),
-    authMiddlewares.checkPetData,
-    updateAuthControllers.addPet
-  );
-
-router
-  .route("/current/pets/remove/:id")
-  .delete(updateAuthControllers.deletePetById);
 
 router.route("/logout").post(authControllers.logout);
 
