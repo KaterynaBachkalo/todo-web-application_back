9e8bb56aa4340739daa8fcbbfd84b8bb577f98b7
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-06 16:21:00 +0300

    add types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from "mongoose";
+import { CreationOptional } from "sequelize";
 
 export interface IUser {
   name?: string;
@@ -10,96 +10,17 @@ export interface IUser {
     candidatePassword: string,
     userPassword: string
   ): Promise<boolean>;
-  phone?: string;
-  avatar?: string;
-  favorites?: string[];
-  viewed?: string[];
-  myPets?: IPet[];
   googleId?: string;
 }
 
-export interface INotice {
-  _id?: ObjectId;
+export interface IContact {
+  id?: CreationOptional<number>;
   name: string;
-  title: string;
-  imgURL: string;
-  species: string;
-  birthday: string;
-  sex: string;
-  category: string;
-  price: number;
-  comment: string;
-  locationId: string;
-  owner: IUser;
-  popularity: number;
-}
-
-export interface IPet {
-  name: string;
-  title: string;
-  imgURL: string;
-  species: string;
-  birthday: string;
-  sex: string;
-}
-
-export interface IMyPet {
-  _id: string;
-  name: string;
-  title: string;
-  imgURL: File;
-  species: string;
-  birthday: string;
-  sex: string;
-  owner: IUser;
-}
-
-export interface ICity {
-  _id: string;
-  useCounty: string;
-  stateEn: string;
-  cityEn: string;
-  countyEn: string;
-}
-
-export interface IFriend {
-  _id: string;
-  title: string;
-  url: string;
-  addressUrl: string;
-  imageUrl: string;
-  address: string;
-  workDays: Array<{
-    _id: string;
-    isOpen: boolean;
-    from: string;
-    to: string;
-  }>;
   phone: string;
-  email: string;
-}
-
-export interface INews {
-  _id: string;
-  imgUrl: string;
-  title: string;
-  text: string;
-  date: string;
-  url: string;
-  id: string;
 }
 
 export interface QueryParams {
-  title?: string | null;
+  name?: string | null;
   page?: number;
   limit?: number;
-  category?: string;
-  sex?: string;
-  species?: string;
-  sort?: string;
-}
-
-export interface IFavorite {
-  _id: string;
-  owner: IUser;
 }
