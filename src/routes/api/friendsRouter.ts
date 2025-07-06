33e2df0417562c5507e92e7276d387cf3753e835
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-06 16:45:43 +0300

    delete

--- src/routes/api/friendsRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getControllers } from "../../controllers";
-
-const router = express.Router();
-
-router.route("/").get(getControllers.getFriends);
-
-export default router;
--- src/routes/api/newsRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getControllers } from "../../controllers";
-
-const router = express.Router();
-
-router.route("/").get(getControllers.getNews);
-
-export default router;
