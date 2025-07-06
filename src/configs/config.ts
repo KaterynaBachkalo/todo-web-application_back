945c79df8192c1a6500bbe46aba1a777389b9e89
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-06 16:21:11 +0300

    add config

---
+++ src/configs/config.ts
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Sequelize } from "sequelize";
+import serverConfig from "./serverConfig";
+
+const sequelize = new Sequelize({
+  database: serverConfig.database,
+  username: serverConfig.username,
+  password: serverConfig.password,
+  host: serverConfig.host,
+  port: serverConfig.port ? Number(serverConfig.port) : undefined,
+  dialect: "mariadb",
+  dialectOptions: {
+    charset: "utf8mb4",
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+    process.exit(1);
+  });
+
+export default sequelize;
