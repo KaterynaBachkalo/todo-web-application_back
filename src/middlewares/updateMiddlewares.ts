6826a8497b53ef9438e49e292537047e5b5a3838
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-25 15:20:44 +0300

    add checkNumber

--- src/middlewares/updateMiddlewares.ts
+++ src/middlewares/updateMiddlewares.ts
@@ -2,19 +2,28 @@ import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import { Contact } from "../models";
 import { HttpError } from "../utils";
+import { jwtServices } from "../services";
 
-const checkContactId = catchAsync(
+const checkNumber = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
+    const { phone } = req.body;
 
-    const idExists = await Contact.findByPk(id);
+    const accessToken =
+      req.headers.authorization?.startsWith("Bearer ") &&
+      req.headers.authorization.split(" ")[1];
 
-    if (idExists) throw new HttpError(409, "Notice already exists");
+    const userId = accessToken && jwtServices.checkToken(accessToken);
+
+    const phoneExists = await Contact.findOne({
+      where: { phone, user_id: userId },
+    });
+
+    if (phoneExists) throw new HttpError(409, "Notice already exists");
 
     next();
   }
 );
 
 export default {
-  checkContactId,
+  checkNumber,
 };
