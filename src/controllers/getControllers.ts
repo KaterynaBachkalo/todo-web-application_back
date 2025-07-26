e9b2e6922df42242165d4196ae32fca60063e73e
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-26 16:06:22 +0300

    fix

--- src/controllers/getControllers.ts
+++ src/controllers/getControllers.ts
@@ -1,15 +1,26 @@
 import { Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
-import { contactServices } from "../services";
+import { contactServices, jwtServices } from "../services";
+import { Contact } from "../models";
 
 const getContacts = catchAsync(async (req: Request, res: Response) => {
-  const { contacts, totalContacts, page, limit } =
-    await contactServices.getContacts(req.query);
+  const accessToken =
+    req.headers.authorization?.startsWith("Bearer ") &&
+    req.headers.authorization.split(" ")[1];
+
+  const userId = accessToken && jwtServices.checkToken(accessToken);
+
+  console.log("userId", userId);
+
+  const contacts =
+    // await contactServices.getContacts(req.query);
+    await Contact.findAll({ where: { user_id: userId } });
+
   res.status(200).json({
     contacts,
-    totalContacts,
-    page,
-    limit,
+    // totalContacts,
+    // page,
+    // limit,
   });
 });
 
