eda68d765b27e9b5097a322f1af48882d4477e1c
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-21 14:55:39 +0300

    fix

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,17 +1,5 @@
 import getControllers from "./getControllers";
 import updateControllers from "./updateControllers";
-import authControllers from "./authControllers";
-import getAuthControllers from "./getAuthControllers";
-import updateAuthControllers from "./updateAuthControllers";
 import proxyController from "./proxyController";
-import google from "./google";
 
-export {
-  getControllers,
-  updateControllers,
-  authControllers,
-  getAuthControllers,
-  updateAuthControllers,
-  proxyController,
-  google,
-};
+export { getControllers, updateControllers, proxyController };
