8f31c4a599e4887c37019bc873ceabde44f05ea7
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-21 14:55:46 +0300

    fix

--- src/controllers/updateControllers.ts
+++ src/controllers/updateControllers.ts
@@ -1,50 +1,31 @@
 import { Request, Response } from "express";
 
 import catchAsync from "../utils/catchAsync";
-import { Contact } from "../models";
+import { Task } from "../models";
 import { HttpError } from "../utils";
-import { CreationOptional } from "sequelize";
-import { contactServices, jwtServices } from "../services";
+import { taskServices } from "../services";
 
-interface CustomRequest extends Request {
-  user: {
-    id: CreationOptional<number>;
-    email: string;
-    name: string;
-  };
-}
+const addTasks = catchAsync(async (req: Request, res: Response) => {
+  const newTask = await taskServices.addTask(req.body);
 
-const addContacts = catchAsync(async (req: CustomRequest, res: Response) => {
-  const accessToken =
-    req.headers.authorization?.startsWith("Bearer ") &&
-    req.headers.authorization.split(" ")[1];
-
-  const userId = accessToken && jwtServices.checkToken(accessToken);
-
-  if (!userId) {
-    throw new HttpError(401, "Not authorized");
-  }
-
-  const newContact = await contactServices.addContact(userId, req.body);
-
-  res.status(201).json(newContact);
+  res.status(201).json(newTask);
 });
 
-const deleteContacts = catchAsync(async (req: CustomRequest, res: Response) => {
+const deleteTasks = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  const deletedContact = await Contact.destroy({ where: { id: id } });
+  const deletedTask = await Task.destroy({ where: { id: id } });
 
-  if (!deletedContact) {
-    throw new HttpError(404, "Contact not found");
+  if (!deletedTask) {
+    throw new HttpError(404, "Task not found");
   }
 
   res.status(200).json({
-    message: "Your contact is successfully removed",
+    message: "Your task is successfully removed",
   });
 });
 
 export default {
-  addContacts,
-  deleteContacts,
+  addTasks,
+  deleteTasks,
 };
