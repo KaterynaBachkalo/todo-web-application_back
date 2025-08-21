108963d554d5029f925c73a42bd5ea96c1446a6c
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-21 14:55:02 +0300

    fix

--- src/types.ts
+++ src/types.ts
@@ -1,26 +1,14 @@
 import { CreationOptional } from "sequelize";
 
-export interface IUser {
-  name?: string;
-  password: string;
-  email: string;
-  accessToken?: string;
-  refreshToken?: string;
-  checkPassword(
-    candidatePassword: string,
-    userPassword: string
-  ): Promise<boolean>;
-  googleId?: string;
-}
-
-export interface IContact {
+export interface ITask {
   id?: CreationOptional<number>;
-  name: string;
-  phone: string;
+  text: string;
+  status: "done" | "undone";
+  priority: number;
 }
 
 export interface QueryParams {
-  name?: string | null;
-  page?: number;
-  limit?: number;
+  text?: string | null;
+  status?: "done" | "undone";
+  priority?: number;
 }
