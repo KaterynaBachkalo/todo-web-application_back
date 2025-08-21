905c622962974d8c091935a1ca4a4e7e573dc210
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-21 14:55:26 +0300

    add getTasks

--- src/controllers/getControllers.ts
+++ src/controllers/getControllers.ts
@@ -1,29 +1,13 @@
 import { Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
-import { contactServices, jwtServices } from "../services";
-import { Contact } from "../models";
+import { Task } from "../models";
 
-const getContacts = catchAsync(async (req: Request, res: Response) => {
-  const accessToken =
-    req.headers.authorization?.startsWith("Bearer ") &&
-    req.headers.authorization.split(" ")[1];
+const getTasks = catchAsync(async (req: Request, res: Response) => {
+  const tasks = await Task.findAll();
 
-  const userId = accessToken && jwtServices.checkToken(accessToken);
-
-  console.log("userId", userId);
-
-  const contacts =
-    // await contactServices.getContacts(req.query);
-    await Contact.findAll({ where: { user_id: userId } });
-
-  res.status(200).json({
-    contacts,
-    // totalContacts,
-    // page,
-    // limit,
-  });
+  res.status(200).json(tasks);
 });
 
 export default {
-  getContacts,
+  getTasks,
 };
