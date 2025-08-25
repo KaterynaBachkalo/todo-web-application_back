02a39559a1e37311dfec4cd04bd9e4fdc89a172e
KaterynaBachkalo <kozlovets88@gmail.com>
2025-08-25 16:33:17 +0300

    fix

--- src/configs/serverConfig.ts
+++ src/configs/serverConfig.ts
@@ -3,12 +3,7 @@ dotenv.config();
 
 const serverConfig = {
   PORT: process.env.PORT || 4000,
-  jwtSecret: process.env.JWT_SECRET ?? "secret-phrase",
-  jwtExpires: process.env.JWT_EXPIRES ?? "20h",
   baseUrl: process.env.BASE_URL ?? "http://localhost:4000",
-  cloudinaryName: process.env.CLOUDINARY_NAME ?? "cloudinaryName",
-  cloudinaryKey: process.env.CLOUDINARY_KEY ?? "12345",
-  cloudinarySecret: process.env.CLOUDINARY_SECRET ?? "cloudinarySecret",
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
